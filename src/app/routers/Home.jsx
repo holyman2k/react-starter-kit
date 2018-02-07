1b12b3c3f550724a65eb0f24d5dcc77847a37eec
Charlie Wu <c.wu@webfm.net>
2018-02-07 16:58:46 +1100

    more work with scss

--- src/app/routers/Home.jsx
+++ src/app/routers/Home.jsx
@@ -7,19 +7,16 @@ import Modal from "../components/Modal.jsx"
 const Home = ({ greeting, showModal, onPresentModal }) => {
     return (
         <div>
-            <h1>{greeting}</h1>
-
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta aliquam gravida. Nulla lobortis aliquet purus vel venenatis. Aliquam eget porta sapien. Cras sodales metus in nunc porta consectetur. Etiam lobortis ultrices nulla, ac lobortis neque lobortis vitae. Fusce pellentesque posuere dui in iaculis. Mauris elit elit, condimentum quis ex nec, aliquet euismod turpis.
-            </p>
-
-            <p>
-                Etiam interdum urna lacus, at tempor urna aliquet et. Proin at justo varius magna luctus dignissim. Quisque ac dignissim odio. Ut congue arcu ligula, et sollicitudin tellus dictum eget. Vivamus risus massa, posuere nec tristique eget, auctor vitae lorem. Sed viverra neque dui, ut dapibus tellus tempor eu. Curabitur dignissim ante nec iaculis venenatis. Aenean quis eros pulvinar, auctor lorem nec, consectetur leo. Ut sodales nunc pretium justo sodales, quis varius leo luctus.
-            </p>
-
-            <Link to="/Ni Hao" class="btn btn-primary">Ni Hao</Link>
-
-            <button class="btn btn-info" onClick={() => onPresentModal(true)}>Modal</button>
+            <div class="jumbotron">
+                <h1 class="display-4">{greeting}</h1>
+                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr class="my-4" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p class="lead">
+                    <Link to="/Ni Hao" class="btn btn-primary">Ni Hao</Link>
+                    <button class="btn btn-info" onClick={() => onPresentModal(true)}>Modal</button>
+                </p>
+            </div>
 
             <Modal show={showModal} title="Alert" onCancel={() => onPresentModal(false)} onAction={() => onPresentModal(false)} >
                 <h1>Hello world</h1>
