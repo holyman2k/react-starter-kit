d01ae244270613da92cf3a27ef54760764542909
Charlie Wu <c.wu@webfm.net>
2018-11-29 15:49:20 +1100

    lazy loading home route

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,16 +5,16 @@ import { ConnectedRouter } from "react-router-redux";
 import history from "./history";
 import store from "./store.js";
 import Layout from "./routers/Layout.jsx";
-import Home from "./routers/Home.jsx";
 import Loader from "react-loader-advanced";
 import Spinner from "./components/Spinner.jsx";
 import loadable from "react-loadable";
 
-const loading = () => (
-    <Loader show={true} message={<Spinner />}>
-        <div />
-    </Loader>
-);
+const Lazy = loader => {
+    return loadable({
+        loader,
+        loading: () => <Loader show={true} message={<Spinner />} />
+    });
+};
 
 const App = () => {
     return (
@@ -22,11 +22,11 @@ const App = () => {
             <ConnectedRouter history={history}>
                 <Layout>
                     <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/animation" component={loadable({ loader: () => import("./routers/Animation.jsx"), loading })} />
-                        <Route exact path="/popups" component={loadable({ loader: () => import("./routers/Popups.jsx"), loading })} />
-                        <Route exact path="/form" component={loadable({ loader: () => import("./routers/Form.jsx"), loading })} />
-                        <Route exact path="/:greeting" component={Home} />
+                        <Route exact path="/" component={Lazy(() => import("./routers/Home.jsx"))} />
+                        <Route exact path="/animation" component={Lazy(() => import("./routers/Animation.jsx"))} />
+                        <Route exact path="/popups" component={Lazy(() => import("./routers/Popups.jsx"))} />
+                        <Route exact path="/form" component={Lazy(() => import("./routers/Form.jsx"))} />
+                        <Route exact path="/:greeting" component={Lazy(() => import("./routers/Home.jsx"))} />
                     </Switch>
                 </Layout>
             </ConnectedRouter>
