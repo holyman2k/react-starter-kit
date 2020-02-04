ccbb35f0254dacc34b8b25269d7f9888613c31f1
Charlie Wu <c.wu@webfm.net>
2020-02-04 14:50:26 +1100

    implemented custom busy indicator

--- src/app/components/Loader.jsx
+++ src/app/components/Loader.jsx
@@ -1,30 +1,17 @@
 import React from "react";
 import Spinner from "./Spinner.jsx";
-// import Loader from "@highpoint/react-loader-advanced";
-var Loader = require("react-loader");
 
 export default ({ show }) => {
-	var options = {
-		lines: 10,
-		length: 12,
-		width: 10,
-		radius: 12,
-		scale: 0.8,
-		corners: 1,
-		color: "#333",
-		opacity: 0.25,
-		rotate: 0,
-		direction: 1,
-		speed: 1.3,
-		trail: 60,
-		fps: 20,
-		zIndex: 2e9,
-		top: "50%",
-		left: "50%",
-		shadow: false,
-		hwaccel: true,
-		position: "absolute"
-	};
-
-	return <Loader loaded={!show} options={options} className="spinner"></Loader>;
+	if (!show) {
+		return null;
+	}
+	return (
+		<div class="Loader__background">
+			<div class="Loader__foreground">
+				<div class="Loader__message">
+					<Spinner />
+				</div>
+			</div>
+		</div>
+	);
 };
--- src/app/components/Spinner.jsx
+++ src/app/components/Spinner.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Spinner = () => {
-    return (
-        <div class="spinner-wrapper">
-            <div class="spinner">
-                <div class="bounce1" />
-                <div class="bounce2" />
-                <div class="bounce3" />
-            </div>
-        </div>
-    );
+	return (
+		<div class="spinner-wrapper">
+			<div class="spinner">
+				<div class="bounce1" />
+				<div class="bounce2" />
+				<div class="bounce3" />
+			</div>
+		</div>
+	);
 };
 
 export default Spinner;
